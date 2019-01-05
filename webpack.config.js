bf6bb4b750130b5edde5ae68ed11ade678c79435
Cameron Edwards <edwards.cameron.pc@gmail.com>
2019-01-05 15:32:21 -0700

    update build process

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,36 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: './index.js',
+  mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
     ]
   },
+  resolve: {
+    modules: [
+      path.resolve('src'),
+      path.resolve('node_modules'),
+    ]
+  },
+  devtool: 'eval-source-map',
   stats: {
     colors: true
   },
-  //devtool: 'source-map',
+  devServer: {
+    hot: true,
+    inline: true,
+    publicPath: '/dist/',
+    historyApiFallback: true,
+  },
 };
